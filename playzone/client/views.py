a290a3c52221ee68931b710bfc5d81aba68451a2
Abhishek Suresh <chuninpeter@yahoo.com>
2018-03-10 16:53:20 +0530

    index

--- playzone/client/views.py
+++ playzone/client/views.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+from django.http import Http404, HttpResponseRedirect, HttpResponse
+from attendance.models import slot,ground
+from django.shortcuts import render
+
+def index(request):
+    slist=slots.objects.filter()
+    return render(request,'client/landing.html',{'slots':slist})
+
+def landing(request):
+    #if request.method == 'POST':
+    #    location=request.POST['location']
+    #    slist=slot.objects.filter(location=location)
+    return render(request,'client/landing.html',{'slots':slist,})
+    
+def addplayer(request):
 
 # Create your views here.
