2c1e70b50512f2d3a68c82284042282aa97ddbb1
adershmanoj <adershmanoj@gmail.com>
2018-03-10 23:37:59 +0530

    Added event filtering

--- static/js/index.js
+++ static/js/index.js
@@ -2,10 +2,6 @@ $(document).ready(function(){
 	if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(showPosition);
   }
-	var my_vars = JSON.parse(document.getElementsByTagName('body')[0].getAttribute('data-js-vars') || '{}');
-			 console.log(my_vars);
-	//var a={{json_list}}
-	//console.log("hello"+a);
 	function showPosition(position){
 		$.getJSON('https://crossorigin.me/https://geoip-db.com/json/', function(data) {
 			if(data.city !== null)
